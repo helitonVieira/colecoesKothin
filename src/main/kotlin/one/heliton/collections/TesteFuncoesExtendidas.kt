6ed16ca9b7259632d683e0656402401a0faf4708
heliton Vieira <helitondba@gmail.com>
2021-03-10 22:18:16 -0300

    Funcoes extendidas

---
+++ src/main/kotlin/one/heliton/collections/TesteFuncoesExtendidas.kt
@@ -0,0 +1,14 @@
+package one.heliton.collections
+
+fun main() {
+    val salarios = arrayOf(
+        2000.toBigDecimal(),
+        3000.toBigDecimal(),
+        4000.toBigDecimal())
+
+    println("--------------------------------")
+    println(salarios.somatoria())
+
+    println("--------------------------------")
+    println(salarios.media())
+}
